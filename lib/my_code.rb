fe0c4d06b416a8e30de961020c2ae49103bbb463
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-12 01:17:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map_to_negativize(array)
   negativized_array = []
   i = 0
   while i < array.length do
-    negativized_array << array[i]*-1
+    negativized_array << array[i]*(-1)
   end
   negativized_array
 end
