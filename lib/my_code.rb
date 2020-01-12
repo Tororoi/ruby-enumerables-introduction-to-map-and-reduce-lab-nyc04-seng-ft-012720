ca8d52eae84b2cec57f6f52d9f514000e33a9e6d
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-12 01:41:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,5 +54,14 @@ def reduce_to_all_true(array)
     return false if !array[i]
     i+=1
   end
-  true
+  return true
+end
+
+def reduce_to_any_true(array)
+  i = 0
+  while i < array.length do
+    return true if !!array[i]
+    i+=1
+  end
+  return false
 end
